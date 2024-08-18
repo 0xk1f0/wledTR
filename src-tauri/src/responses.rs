067c0b0cb87be9d4b87b038b33f76ace9da55477
0xk1f0 <dev@k1f0.dev>
2024-08-18 21:36:05 +0200

    fix(ci): migrate to new permission model and remove unused stuff

--- src-tauri/src/responses.rs
+++ src-tauri/src/responses.rs
@@ -11,41 +11,6 @@ pub struct PowerResponse {
 }
 
 #[derive(Deserialize)]
-pub struct Wifi {
-    pub bssid: String,
-    pub rssi: isize,
-    pub signal: usize,
-    pub channel: usize,
-}
-
-#[derive(Deserialize)]
-pub struct Segments {
-    pub start: usize,
-    pub stop: usize,
-    pub length: usize,
-    pub col: Vec<Vec<(u8, u8, u8)>>,
-}
-
-#[derive(Deserialize)]
-pub struct StateResponse {
-    pub on: bool,
-    pub bri: usize,
-    pub ps: usize,
-    pub seg: Vec<Segments>,
-}
-
-#[derive(Deserialize)]
 pub struct InfoResponse {
-    pub ver: String,
-    pub name: String,
-    pub arch: String,
-    pub core: String,
-    pub freeheap: usize,
-    pub uptime: usize,
-    pub opt: usize,
-    pub brand: String,
-    pub product: String,
-    pub mac: String,
     pub ip: String,
-    pub wifi: Wifi,
 }
