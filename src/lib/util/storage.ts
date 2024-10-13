597212266e711d50e5d39e6bf44dcfccda21d5d1
0xk1f0 <dev@k1f0.dev>
2024-10-13 17:47:10 +0200

    fix(build): fully convert to Tauri 2.0 bits

--- src/lib/util/storage.ts
+++ src/lib/util/storage.ts
@@ -1,4 +1,5 @@
-import { Store } from '@tauri-apps/plugin-store';
+import { createStore } from '@tauri-apps/plugin-store';
+import type { Store } from '@tauri-apps/plugin-store';
 import type { Device, StoreData } from '$lib/types/store';
 
 class StorageHandler {
@@ -8,84 +9,102 @@ class StorageHandler {
         this.#path = path;
     }
 
-    private async load(store: Store): Promise<StoreData> {
-        let query = await store.get<StoreData>('config');
+    private async load(store: Promise<Store>): Promise<StoreData> {
+        let query = await (await store).get<StoreData>('config');
         if (query === null) {
             query = { devices: [] };
         }
         return query;
     }
 
-    private add(store: Store) {
-        const single = async (store: Store, device: Device) => {
-            let data = await store.get<StoreData>('config') || { devices: [] };
-            data.devices.push(device)
-            await store.set('config', data);
+    private add(store: Promise<Store>) {
+        const single = async (store: Promise<Store>, device: Device) => {
+            let data = (await (await store).get<StoreData>('config')) || { devices: [] };
+            data.devices.push(device);
+            await (await store).set('config', data);
             return {
-                save: () => {
-                    return store.save();
+                save: async () => {
+                    return await (await store).save();
                 }
             };
-        }
+        };
 
-        const multiple = async (store: Store, devices: Device[]) => {
-            let data = await store.get<StoreData>('config') || { devices: [] };
+        const multiple = async (store: Promise<Store>, devices: Device[]) => {
+            let data = (await (await store).get<StoreData>('config')) || { devices: [] };
             for (let device of devices) {
                 data.devices.push(device);
             }
-            await store.set('config', data);
+            await (await store).set('config', data);
             return {
-                save: () => {
-                    return store.save();
+                save: async () => {
+                    return await (await store).save();
                 }
             };
-        }
+        };
 
         return {
-            single: (device: Device) => { return single(store, device) },
-            multiple: (devices: Device[]) => { return multiple(store, devices) }
+            single: async (device: Device) => {
+                return await single(store, device);
+            },
+            multiple: async (devices: Device[]) => {
+                return await multiple(store, devices);
+            }
         };
     }
 
-    private remove(store: Store) {
-        const single = async (store: Store, device: Device) => {
-            let data = await store.get<StoreData>('config') || { devices: [] };
+    private remove(store: Promise<Store>) {
+        const single = async (store: Promise<Store>, device: Device) => {
+            let data = (await (await store).get<StoreData>('config')) || { devices: [] };
             data.devices.splice(data.devices.indexOf(device), 1);
-            await store.set('config', data);
+            await (await store).set('config', data);
             return {
-                save: () => {
-                    return store.save();
+                save: async () => {
+                    return (await store).save();
                 }
             };
-        }
+        };
 
-        const multiple = async (store: Store, devices: Device[]) => {
-            let data = await store.get<StoreData>('config') || { devices: [] };
+        const multiple = async (store: Promise<Store>, devices: Device[]) => {
+            let data = (await (await store).get<StoreData>('config')) || { devices: [] };
             for (let device of devices) {
                 data.devices.splice(data?.devices.indexOf(device), 1);
             }
-            await store.set('config', data);
+            await (await store).set('config', data);
             return {
-                save: () => {
-                    return store.save();
+                save: async () => {
+                    return (await store).save();
                 }
             };
-        }
+        };
 
         return {
-            single: (device: Device) => { return single(store, device) },
-            multiple: (devices: Device[]) => { return multiple(store, devices) }
+            single: (device: Device) => {
+                return single(store, device);
+            },
+            multiple: (devices: Device[]) => {
+                return multiple(store, devices);
+            }
         };
     }
 
     public open() {
-        let store = new Store(this.#path);
+        let store = createStore(this.#path);
         return {
-            load: () => { return this.load(store) },
-            add: () => { return this.add(store) },
-            remove: () => { return this.remove(store) },
-            clear: () => { return store.clear() },
-            save: () => { return store.save() }
+            load: () => {
+                return this.load(store);
+            },
+            add: () => {
+                return this.add(store);
+            },
+            remove: () => {
+                return this.remove(store);
+            },
+            clear: async () => {
+                return (await store).clear();
+            },
+            save: async () => {
+                return (await store).save();
+            }
         };
     }
 }
